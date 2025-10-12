6af115b8152fdf5278bfcb8610201220244b28d0
AdemBU <61334304+AdemBU@users.noreply.github.com>
2025-10-12 22:39:39 +0300

    Member Dashboard sayfasında yenileme ve düzenlemeler yapıldı

---
+++ Traversal/ViewComponents/MemberLayout/_MemberLayoutSideBar.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Org.BouncyCastle.Asn1.Crmf;
+
+namespace Traversal.ViewComponents.MemberLayout
+{
+    public class _MemberLayoutSideBar : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View();
+        }
+    }
+}
